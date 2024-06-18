5a4341ca9b7654e558f1c503fc195ab9343e8f7d
huangshicheng <1143347408@qq.com>
2024-06-18 23:22:46 +0800

    日志开发

---
+++ go.mod
@@ -0,0 +1,15 @@
+module huangshic/mlog
+
+go 1.21.0
+
+require (
+	github.com/opentracing/opentracing-go v1.2.0
+	go.opentelemetry.io/otel/trace v1.27.0
+	go.uber.org/zap v1.27.0
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
+)
+
+require (
+	go.opentelemetry.io/otel v1.27.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"huangshic/mlog/mlog"
+	"huangshic/mlog/mlog/conf"
+	"huangshic/mlog/mlog/zaplog"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+func main() {
+	mlog.SetLogger(zaplog.New(
+		conf.WithProjectName("pro"),
+		conf.WithLogPath("log"),
+		conf.WithLogLevel("info"),
+		conf.WithMaxSize(100),
+		conf.WithMaxAge(7),
+		conf.WithIsStdOut("yes"),
+		conf.WithLogName("test"),
+	))
+	mlog.Info("TTTTest info", zap.String("k", "j"))
+	mlog.Error("test error", zap.String("k", "j"))
+	time.Sleep(10 * time.Second)
+}
